71bce904ed1e1cf827a7cb868a4910d58eda168f
CongSanhLe <congsanhle1999@gmail.com>
2022-12-30 07:45:41 +0700

    [301222] Mongoose Error Handling

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -8,6 +8,18 @@ export default (err, req, res, next) => {
 
   error.message = err.message;
 
+  // Wrong Mongoose Object ID error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  // Handling mongoose Validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     error,
